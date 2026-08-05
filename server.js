ca79702c9eb546a8f7a68aa804ed223fe2ce07a2
VaibhavShukla2004 <vaibhavshukla1110r@gmail.com>
2026-08-05 13:06:04 +0530

    cors added

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 require('dotenv').config();
 const connectDB = require('./config/db');
+const cors = require('cors');
 
 // Connect to MongoDB
 connectDB();
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(cors());
 
 // Define a simple route
 app.get('/', (req, res) => {
