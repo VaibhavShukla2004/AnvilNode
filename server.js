a368b31367a307f1a8d97b79f41313f0bade8b80
VaibhavShukla2004 <vaibhavshukla1110r@gmail.com>
2026-08-05 12:44:07 +0530

    Comments added to Server.js

--- server.js
+++ server.js
@@ -7,10 +7,12 @@ connectDB();
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Define a simple route
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+// Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
