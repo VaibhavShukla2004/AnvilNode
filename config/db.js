c1e4994909e0b3e543f551c7741951ad79ffd856
VaibhavShukla2004 <vaibhavshukla1110r@gmail.com>
2026-08-05 12:25:36 +0530

    Removed ping from database connection code

--- config/db.js
+++ config/db.js
@@ -11,13 +11,11 @@ const clientOptions = {
 
 const connectDB = async () => {
     try {
-        console.log("env:" + process.env.MONGO_URI+ "\n\n");
         if (!uri) {
             console.error("MONGO_URI not found in environment variables.");
             process.exit(1);
         }
         await mongoose.connect(uri, clientOptions);
-        await mongoose.connection.db.admin().command({ ping: 1 });
         console.log("MongoDB connected successfully!");
     } catch (error) {
         console.error("MongoDB Connection Error:", error);
